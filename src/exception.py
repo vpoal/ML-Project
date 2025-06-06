f79c5057051fa4a44ff7e5731205a2df04c77db1
vpoal <vpoal@academian.com>
2025-06-06 10:09:17 +0530

    update

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-from src.logger import logging
+import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -18,7 +18,5 @@ class CustomException(Exception):
     
     def __str__(self):
         return self.error_message
-    
-
 
         
